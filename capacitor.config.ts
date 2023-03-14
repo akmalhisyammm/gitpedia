710b103e4142db86d6a5af7bae6f4c908745cb7b
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2023-03-14 15:56:36 +0700

    feat: initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'id.ac.umn.muhammadakmalhisyam.ionic.gitpedia',
+  appName: 'Gitpedia',
+  webDir: 'build',
+  bundledWebRuntime: false,
+};
+
+export default config;
