f02030d1611b081969851cc3c2f34695476809f6
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2023-03-16 20:13:55 +0700

    feat: add greeting component for onboarding page

---
+++ src/pages/Onboarding.tsx
@@ -0,0 +1,12 @@
+import { Greeting } from 'components/organisms';
+import Layout from 'components/layout';
+
+const Onboarding = () => {
+  return (
+    <Layout>
+      <Greeting />
+    </Layout>
+  );
+};
+
+export default Onboarding;
