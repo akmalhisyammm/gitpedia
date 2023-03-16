74f2abc49b8a1a2ee3a57df026d1da582c522152
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2023-03-16 20:14:38 +0700

    feat(router): add onboarding page route

---
+++ src/router/index.tsx
@@ -0,0 +1,30 @@
+import { IonRouterOutlet } from '@ionic/react';
+import { Route } from 'react-router-dom';
+
+// import { MainTabs } from 'components/organisms';
+import Onboarding from 'pages/Onboarding';
+// import About from 'pages/About';
+// import Login from 'pages/auth/Login';
+// import Register from 'pages/auth/Register';
+// import Chapter from 'pages/learn/Chapter';
+// import Lesson from 'pages/learn/Lesson';
+// import Quiz from 'pages/learn/Quiz';
+
+const Routes = () => {
+  return (
+    <IonRouterOutlet id="main">
+      <Route exact path="/" component={Onboarding} />
+      {/* <Route path="/about" component={About} />
+      <Route path="/main" component={MainTabs} />
+      <Route path="/auth/login" component={Login} />
+      <Route path="/auth/register" component={Register} />
+      <Route exact path="/learn/:chapterId" component={Chapter} />
+      <Route exact path="/learn/:chapterId/:lessonId" component={Lesson} />
+      <Route path="/learn/:chapterId/:lessonId/quiz" component={Quiz} />
+
+      <Redirect exact from="/auth" to="/auth/login" /> */}
+    </IonRouterOutlet>
+  );
+};
+
+export default Routes;
