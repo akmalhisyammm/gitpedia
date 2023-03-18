ec3a2f936f63137b0b7d2e3b9430085bd7010275
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2023-03-18 22:32:22 +0700

    feat(components): add store card molecule component

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,3 +1,4 @@
 export { default as ChapterCard } from './ChapterCard';
 export { default as InputGroup } from './InputGroup';
 export { default as LessonCard } from './LessonCard';
+export { default as StoreCard } from './StoreCard';
