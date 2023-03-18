d7e176fa45e98dff63655e0603da05e8473a375c
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2023-03-18 22:33:14 +0700

    feat(pages): integrate store components into store page

---
+++ src/pages/main/Store.tsx
@@ -0,0 +1,12 @@
+import { Layout } from 'components/layouts';
+import { StoreList } from 'components/organisms';
+
+const Store = () => {
+  return (
+    <Layout title="Toko">
+      <StoreList />
+    </Layout>
+  );
+};
+
+export default Store;
