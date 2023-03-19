ca73edd7a26da808f29cfa85b0192848123376f9
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2023-03-19 21:07:09 +0700

    feat(components): integrate chapter components into page

---
+++ src/pages/learn/Chapter.tsx
@@ -0,0 +1,12 @@
+import { Layout } from 'components/layouts';
+import { LessonList } from 'components/organisms';
+
+const Chapter = () => {
+  return (
+    <Layout title="Chapter Title">
+      <LessonList />
+    </Layout>
+  );
+};
+
+export default Chapter;
