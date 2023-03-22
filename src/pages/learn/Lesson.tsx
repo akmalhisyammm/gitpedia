ead4946c80bc9eb06ed67caa9fffa6c8490b8a45
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2023-03-22 15:51:45 +0700

    feat(pages): integrate lesson components into page

---
+++ src/pages/learn/Lesson.tsx
@@ -0,0 +1,12 @@
+import { Layout } from 'components/layouts';
+import { LessonContent } from 'components/organisms';
+
+const Lesson = () => {
+  return (
+    <Layout title="Lesson Title">
+      <LessonContent />
+    </Layout>
+  );
+};
+
+export default Lesson;
