e69e62169cd46aaa04cd82141837f2b9628616f1
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2023-03-30 22:52:40 +0700

    refactor: simplify firebase lib

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,28 +1,23 @@
-import { getApp, getApps, initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import { getApps, getApp, initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore, collection } from 'firebase/firestore';
 
-import { firebaseConfig } from 'config';
-import { IChapter } from 'types/chapter';
-import { IStoreItem } from 'types/store';
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
 
 const firebaseApp = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const firebaseAuth = getAuth(firebaseApp);
 const firestore = getFirestore(firebaseApp);
 
 const chaptersCollection = collection(firestore, 'chapters');
 const storesCollection = collection(firestore, 'stores');
+const usersCollection = collection(firestore, 'users');
 
-const getAllChapters = async () => {
-  const snapshot = await getDocs(chaptersCollection);
-  const chapters = snapshot.docs.map((doc) => ({ ...(doc.data() as IChapter) }));
-
-  return chapters;
-};
-
-const getAllStoreItems = async () => {
-  const snapshot = await getDocs(storesCollection);
-  const stores = snapshot.docs.map((doc) => ({ ...(doc.data() as IStoreItem) }));
-
-  return stores;
-};
-
-export { getAllChapters, getAllStoreItems };
+export { firebaseAuth, firestore, chaptersCollection, storesCollection, usersCollection };
