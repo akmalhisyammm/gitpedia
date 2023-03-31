a695e412eae624a57d8078708560c9add4d0085a
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2023-03-31 13:42:42 +0700

    refactor(pages): adjust chapter and lesson pages

--- src/pages/learn/Chapter.tsx
+++ src/pages/learn/Chapter.tsx
@@ -1,10 +1,22 @@
+import { useContext } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { ChapterContext } from 'contexts/chapter';
 import { Layout } from 'components/layouts';
 import { LessonList } from 'components/organisms';
 
+type ChapterParams = {
+  chapterId: string;
+};
+
 const Chapter = () => {
+  const { chapterId } = useParams<ChapterParams>();
+
+  const chapterCtx = useContext(ChapterContext);
+
   return (
-    <Layout title="Chapter Title">
-      <LessonList />
+    <Layout title={chapterCtx.chapters[Number(chapterId) - 1]?.title}>
+      {!!chapterCtx.chapters.length && <LessonList chapterId={Number(chapterId)} />}
     </Layout>
   );
 };
--- src/pages/learn/Lesson.tsx
+++ src/pages/learn/Lesson.tsx
@@ -1,10 +1,58 @@
+import { useContext, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+
+import { ChapterContext } from 'contexts/chapter';
 import { Layout } from 'components/layouts';
-import { LessonContent } from 'components/organisms';
+import { LessonContent, QuizModal } from 'components/organisms';
+import { fyShuffle } from 'utils/fyShuffle';
+
+import type { IQuiz } from 'types/chapter';
+
+type LessonParams = {
+  chapterId: string;
+  lessonId: string;
+};
 
 const Lesson = () => {
+  const { chapterId, lessonId } = useParams<LessonParams>();
+
+  const [isQuizStarted, setIsQuizStarted] = useState<boolean>(false);
+  const [shuffledQuiz, setShuffledQuiz] = useState<IQuiz[]>([]);
+
+  const history = useHistory();
+  const chapterCtx = useContext(ChapterContext);
+
+  const handleStartQuiz = () => {
+    const shuffleQuiz = fyShuffle(
+      chapterCtx.chapters[Number(chapterId) - 1].lessons[Number(lessonId) - 1].quiz
+    );
+
+    setShuffledQuiz(shuffleQuiz);
+    setIsQuizStarted(true);
+  };
+
+  const handleFinishQuiz = () => {
+    setIsQuizStarted(false);
+    history.replace(`/learn/${chapterId}`);
+  };
+
   return (
-    <Layout title="Lesson Title">
-      <LessonContent />
+    <Layout title={chapterCtx.chapters[Number(chapterId) - 1]?.lessons[Number(lessonId) - 1].title}>
+      {!!chapterCtx.chapters.length && (
+        <LessonContent
+          content={chapterCtx.chapters[Number(chapterId) - 1].lessons[Number(lessonId) - 1].content}
+          onStartQuiz={handleStartQuiz}
+        />
+      )}
+      {!!shuffledQuiz.length && (
+        <QuizModal
+          data={shuffledQuiz.slice(0, 3)}
+          chapterId={Number(chapterId)}
+          lessonId={Number(lessonId)}
+          isOpen={isQuizStarted}
+          onFinish={handleFinishQuiz}
+        />
+      )}
     </Layout>
   );
 };
