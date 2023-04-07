60e27fc6df9ce51e7be8f528dc677ebaaf7cead5
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2023-04-07 14:57:03 +0700

    feat(constants): add several constant images url

---
+++ src/constants/images.ts
@@ -0,0 +1,23 @@
+/* AVATAR */
+export const DEFAULT_AVATAR_URL =
+  'https://firebasestorage.googleapis.com/v0/b/gitpedia-prod.appspot.com/o/icons%2Favatars%2Fdefault.png?alt=media&token=1f9a26a2-2291-4c54-aabb-48ec373e4587';
+
+/* ICON */
+export const LOGO_ICON_URL =
+  'https://firebasestorage.googleapis.com/v0/b/gitpedia-prod.appspot.com/o/icons%2Fgitpedia.png?alt=media&token=f081f8e1-5d44-4e30-8c8e-bad734d6152c';
+export const COIN_ICON_URL =
+  'https://firebasestorage.googleapis.com/v0/b/gitpedia-prod.appspot.com/o/icons%2Fcoin.png?alt=media&token=5c8b69ce-6968-46c8-a690-fec5aae0a804';
+
+/* ILLUSTRATION */
+export const CODING_ILLUSTRATION_URL =
+  'https://firebasestorage.googleapis.com/v0/b/gitpedia-prod.appspot.com/o/illustrations%2Fcoding.svg?alt=media&token=11d65fcb-d8c7-4442-8e43-cb8b85349fde';
+export const SECURITY_ILLUSTRATION_URL =
+  'https://firebasestorage.googleapis.com/v0/b/gitpedia-prod.appspot.com/o/illustrations%2Fsecurity-on-pana.svg?alt=media&token=77ccc0a1-528a-453c-a7f7-32c6e76a4c2e';
+export const VERIFIED_ILLUSTRATION_URL =
+  'https://firebasestorage.googleapis.com/v0/b/gitpedia-prod.appspot.com/o/illustrations%2Fproduct-quality-pana.svg?alt=media&token=d5cf31d5-baf8-465f-a8aa-78340d3218b9';
+export const EXPIRED_ILLUSTRATION_URL =
+  'https://firebasestorage.googleapis.com/v0/b/gitpedia-prod.appspot.com/o/illustrations%2Fstress-pana.svg?alt=media&token=47fc9c1f-0686-4702-b196-e12e72184e07';
+export const PASSED_ILLUSTRATION_URL =
+  'https://firebasestorage.googleapis.com/v0/b/gitpedia-dev.appspot.com/o/illustrations%2Fwinners-pana.svg?alt=media&token=5fbafa20-f03d-4680-8123-398b2d9864ee';
+export const FAILED_ILLUSTRATION_URL =
+  'https://firebasestorage.googleapis.com/v0/b/gitpedia-dev.appspot.com/o/illustrations%2Fanxiety-pana.svg?alt=media&token=56804564-a5d6-4ee6-bf5d-9e233146446a';
